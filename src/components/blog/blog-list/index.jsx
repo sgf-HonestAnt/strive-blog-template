c3e11fbf09f386050993a2a03f6953abf7c4a2d0
Sarah Fisher <82496944+sgf-HonestAnt@users.noreply.github.com>
2021-08-11 15:15:47 +0100

    posts loading from backend

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -1,17 +1,41 @@
 import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
-import posts from "../../../data/posts.json";
-export default class BlogList extends Component {
+// import posts from "../../../data/posts.json";
+export default class BlogList extends Component { 
+  state = {
+    posts: [],
+    loading: true,
+  };
+  componentDidMount() {
+    this.fetchBlogPosts();
+  }
+  fetchBlogPosts = async () => {
+    try {
+      let response = await fetch(
+        "http://localhost:3333/blogs"
+        // `${ENDPOINT_BLOGS}`
+      );
+      let postsJson = await response.json();
+      this.setState({ posts: postsJson, loading: false })
+    } catch (error) {
+      console.log("error")
+    }
+  };
+  componentDidUpdate(prevProps, prevState) {
+  }
+
   render() {
     return (
-      <Row>
-        {posts.map((post) => (
-          <Col md={4} style={{ marginBottom: 50 }}>
-            <BlogItem key={post.title} {...post} />
-          </Col>
-        ))}
-      </Row>
+      this.state.loading === false ? (
+        <Row>
+          {this.state.posts.map((post) => (
+            <Col md={4} key={post._id} style={{ marginBottom: 50 }}>
+              <BlogItem {...post} />
+            </Col>
+          ))}
+        </Row>
+       ) : (<Row></Row>)
     );
   }
 }
